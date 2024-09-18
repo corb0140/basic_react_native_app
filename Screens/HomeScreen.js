3c92d2cea5e131dda671545582c6234a8134c1b8
Mark Corbin <markpc1608@gmail.com>
2024-09-18 15:27:47 -0400

    removed style from button, added a view

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,21 +1,22 @@
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Text, Button, StyleSheet } from "react-native";
+import { Text, Button, StyleSheet, View } from "react-native";
 
 export default function HomeScreen({ navigation }) {
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.textStyle}>Basic React Native App</Text>
-      <Button
-        style={styles.buttonContainer}
-        title="Go to Gallery Page"
-        onPress={() => navigation.navigate("Gallery")}
-      />
-      <Button
-        style={styles.buttonContainer}
-        title="Go to Games Page"
-        onPress={() => navigation.navigate("Games")}
-        color={"#f19"}
-      />
+
+      <View style={styles.buttonView}>
+        <Button
+          title="Go to Gallery Page"
+          onPress={() => navigation.navigate("Gallery")}
+        />
+        <Button
+          title="Go to Games Page"
+          onPress={() => navigation.navigate("Games")}
+          color={"#f19"}
+        />
+      </View>
     </SafeAreaView>
   );
 }
@@ -25,10 +26,15 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    height: "100%",
   },
   textStyle: {
     fontSize: 20,
     fontWeight: "bold",
     paddingBottom: 50,
+  },
+  buttonView: {
+    flex: 1,
+    gap: 10,
   },
 });
